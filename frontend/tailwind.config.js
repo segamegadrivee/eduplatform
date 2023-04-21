944a90d87921522247ac1f254638b61cfe4f732d
sega <sergohack2015@gmail.com>
2023-04-21 20:43:36 +0300

    courses

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  content: ['./index.html',
+    './src/pages/*.html'],
   theme: {
     extend: {},
   },
